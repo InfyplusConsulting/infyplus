8e0a60abfc0665474cc1f4abd0e41bc255bec57f
Devansh Rajput <devanshrajput032006@gmail.com>
2026-07-02 04:02:18 +0530

    vercel config deleted

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,17 +21,6 @@ export default withAuth(
   }
 );
 
-// UPDATED MATCHER
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    "/admin/:path*"
-  ],
+  matcher: ["/admin/:path*"],
 };
